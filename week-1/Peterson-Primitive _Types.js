0bf3d13b01b6be5fac9242c624c01770907497bb
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-02 14:36:22 -0500

    Output Code Added

--- week-1/Peterson-Primitive _Types.js
+++ week-1/Peterson-Primitive _Types.js
@@ -15,14 +15,17 @@
 // variable declaration and assignment goes here ...
 var firstName = "Heather";
 var lastName = "Peterson";
-var Age = "31";
+var Age = "Age 31";
 var Street ="1234 Cherry Lane";
 var City = "Pittsburgh";
 var State = "PA";
 var zipCode = "15204";
 
 // output 
-console.log('\n');
+console.log(firstName,lastName);
+console.log(Age);
+console.log(Street);
+console.log(City,State,zipCode);
 
 
 // end program 
