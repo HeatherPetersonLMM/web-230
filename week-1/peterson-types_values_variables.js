9c0a19f6f463b81c045498e9bf2b1456bda2a844
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-02 15:34:48 -0500

    adding employee first names

---
+++ week-1/peterson-types_values_variables.js
@@ -0,0 +1,46 @@
+/*
+============================================
+; Title:  Assignment 1.5
+; Author: Albert Einstein
+; Date:   25 June 2017
+; Modified By: <Your name>
+; Description: This program demonstrates the
+;   use of JavaScript types, values, and 
+;   and variables in an application.
+;===========================================
+*/ 
+
+/*
+    The code below builds a properly formatted
+    header and must be included in all applications
+    you write. In subsequent week's we will build this
+    functionality into a function and place it in a separate file.
+*/
+
+var myFirstName = "Heather";
+var myLastName = "Peterson";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+
+// start of program - your code goes below this line
+
+
+var firstName1 = "Bear";
+var firstName2 = "Lily";
+var firstName3 = "Russ";
+var firstName4 = "Lucy";
+var firstNsme5 = "Bob";
+
+
+// end of program 
+
+
+
+
+ 
