de5b76899a8e4c283d7a7b180568ed532e82aad0
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-02 14:15:46 -0500

    Committing Primitive Types

---
+++ week-1/Peterson-Primitive _Types.js
@@ -0,0 +1,28 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Heather";
+var lastName = "Peterson";
+var Age = "31";
+var Street ="1234 Cherry Lane";
+var City = "Pittsburgh";
+var State = "PA";
+var zipCode = "15204";
+
+// output 
+console.log("Hello World, my name is Heather");
+
+
+// end program 
