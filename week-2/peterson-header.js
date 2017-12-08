e17ad603e90fbf36c8ea7d78aa25c7723ff73575
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-08 11:44:34 -0500

    Adding headers

---
+++ week-2/peterson-header.js
@@ -0,0 +1,6 @@
+var header = require('../header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Heather", "Peterson", "Exercise 2.2")); 
+console.log("\n");
