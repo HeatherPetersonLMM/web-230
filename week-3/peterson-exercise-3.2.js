1a5618171bd63a19815ea5b47fe93aa6441e31e6
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-17 13:22:38 -0500

    correct coding with var A and var B output for Match

---
+++ week-3/peterson-exercise-3.2.js
@@ -0,0 +1,62 @@
+var header = require('../header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 3.2
+    Today's Date
+
+    // output from the match() function 
+    false
+    true 
+
+    // output from the if...else blocks 
+    Truck and Car do not match!
+    Bike and Bike do match!
+    Four and Three do not match!
+*/ 
+
+
+// functions 
+function Heather() {
+    return Heather.Peterson
+}
+
+var A = 2;
+var B = 3;
+function match() {
+    if(2 == 3) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function logMismatch() {
+}
+
+function logMatch() {
+}
+
+// six (6) test variables 
+
+
+
+// Output from the match() function...
+console.log(Heather()); 
+console.log(header);
+console.log("\n");
+console.log(match("A", "B"));
+console.log(match(2, 2)); 
+ 
+
+
+
+
+// Conditional "if...else" statements. Include checks for all six (6) test variables 
+
+
+// end program 
