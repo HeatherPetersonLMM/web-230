5ab1ca11ef09448efc6d0a4f1777894fd7652a58
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-17 22:24:00 -0500

    Finished code for exercise 3.2

--- week-3/peterson-exercise-3.2.js
+++ week-3/peterson-exercise-3.2.js
@@ -47,8 +47,28 @@ function logMatch(Bike,Bike) {
     }
 }
 
-// six (6) test variables 
+function logMismatch2(Four,Three) {
+    if (Four === Three) {
+        return 'This is a match.';
+        }
+        else {
+        return 'Four and Three do not match!';
+        }
+    }
 
+
+// six (6) test variables 
+var msg ='';
+var quantity;
+var code;
+var price;
+var total;
+var name;
+quantity = 300;
+code = 1001;
+price = 20.99;
+total = quantity + price;
+name = code = 'Hammer';
 
 
 // Output from the match() function... 
@@ -56,9 +76,24 @@ console.log(match("A", "B"));
 console.log(match(2, 2)); 
 console.log(logMismatch("Truck", "Car"));
 console.log(logMatch("Bike","Bike"));
+console.log(logMismatch2("Four","Three"));
 
 // Conditional "if...else" statements. Include checks for all six (6) test variables 
-
+if (msg == quantity) {
+    console.log(quantity);
+} else if (msg !== code);{
+    console.log(code);
+}
+if (msg == price) {
+    console.log(price);
+} else if (msg == total);{
+    console.log(total);
+}
+if (msg == name) {
+    console.log(name,code);
+} else if (msg == code);{
+    console.log(code);
+}
 
 
 
