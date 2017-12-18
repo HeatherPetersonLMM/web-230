12a5adbeb50aaa8f7509f1fb4700d77d91996dc5
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-17 21:29:55 -0500

    Adjusted and Corrected Code

--- week-3/peterson-exercise-3.2.js
+++ week-3/peterson-exercise-3.2.js
@@ -18,79 +18,35 @@ var header = require('../header.js');
     Bike and Bike do match!
     Four and Three do not match!
 */ 
-// variables
-var A = 'Truck';
-var B = 'Car';
-var C = 'Bike';
-var D = 'Bike';
-var E = 'Four';
-var F = 'Three';
-var choice = 'Truck';
 
 // functions 
-function Heather() {
-    return Heather.Peterson
-}
-
-
-function match() {
-    //var A = 2;
-    //var B = 3;
-    if(A == B) {
-        return true;
-    } else {
-        return false;
+function match(A,B) {
+    if (A === B) {
+    return true;
+    }
+    else {
+    return false;
     }
 }
 
 function logMismatch() {
-    //var A = 2;
-    //var B = 3;
-    var msg = '';
-
-    if(A != B) {
-        return msg = 'This is not a match.';
-    } else {
-        return msg = 'This is a match.';
-    }
 }
 
 function logMatch() {
-    //var A = 2;
-    //var B = 2;
-    var msg2 = '';
-
-    if(A == B) {
-        return msg2 = 'This is a match.';
-    } else {
-        return msg2 = 'This is not a match.';
-    }
 }
 
-// Output from the match() function...
-console.log(Heather()); 
-console.log(header);
-console.log("\n");
+// six (6) test variables 
 
-console.log(match("A","B"));
-console.log(logMismatch("msg"));
-console.log(logMatch("msg2"));
- 
+
+
+// Output from the match() function... 
+console.log(match("A", "B"));
+console.log(match(2, 2)); 
+
 
 // Conditional "if...else" statements. Include checks for all six (6) test variables 
 
-if (choice == "A") {
-    console.log("A");
-} else if (choice == "B") {
-    console.log("B");
 
-} else if (choice == "C") {
-    console.log("Winner Winner!");
 
-} else if (choice == "D") {
-    console.log("D");
-} else {
-    console.log("Invalid choice");
-}
 
 // end program 
