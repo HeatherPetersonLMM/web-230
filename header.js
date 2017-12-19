154f395e8445a9d87f838e78da60593a8e3a1e00
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-19 10:46:45 -0500

    changed exercise number

--- header.js
+++ header.js
@@ -23,7 +23,7 @@ if (dd<10) {
   }
   
   today = mm + '-' + dd + '-' + yyyy
-  var assignmentNum = "Exercise 3.3";
+  var assignmentNum = "Exercise 4.2";
   
   var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
       + assignmentNum + "\nDate: " + today;
