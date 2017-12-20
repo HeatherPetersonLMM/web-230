034c4789f46d415ca01b272e50243b0ed10712e1
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-20 14:04:46 -0500

    discussion -4.1-example

---
+++ peterson-discussion-w4.js
@@ -0,0 +1,17 @@
+// start program
+
+// function
+function goTeam(){
+    var sports = ["football", "hockey", "basketball", "soccer"];  // Here is an array of 4 elements
+    sports.sort();    // Alphabetizes the elements
+    for(var i = 0; i < sports.length; i++) {   // This creates a loop of the elements
+      console.log(sports[i]);
+    }
+  }
+
+// Output from the getFruit() function
+console.log(goTeam());  // The output displays the array elements
+
+
+
+// end program
