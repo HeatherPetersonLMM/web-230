b6a603fcff42ace965d9195e0386dd55d7e21b18
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-19 19:48:39 -0500

    Added header and code attributions

---
+++ week-4/peterson-exercise-4.3.js
@@ -0,0 +1,10 @@
+var header = require('../header.js'); 
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   19 December 2017
+; Modified By: Heather Peterson
+; Description: Exercise 4.3 - Filtering
+;===========================================
+*/ 
