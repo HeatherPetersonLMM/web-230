48a56beb83d6a6c272dc6cc6a92309145d9d0d6c
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-22 09:38:25 -0500

    added starter code and header

---
+++ week-4/peterson-assignment-4.4.js
@@ -0,0 +1,43 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Assignment 4.4
+ Today's Date
+
+ -- ORIGINAL ARRAY --
+ Alabama
+ Nebraska
+ Iowa
+ California
+ Nevada
+
+ -- SORTED ARRAY --
+ Alabama
+ California
+ Iowa
+ Nebraska
+ Nevada
+
+ -- SELECTED VALUE --
+ Iowa
+
+ */
+
+// start program
+
+
+// function
+function getValue(x, val) {
+
+}
+
+
+// Output
+
+
+
+
+// end program
