528065d1b412f14b55f2bbf7db73e570b1aa77b7
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-22 10:52:42 -0500

    corrected code for exercise 3.2 - false/true

--- week-3/peterson-exercise-3.2.js
+++ week-3/peterson-exercise-3.2.js
@@ -1,5 +1,15 @@
 var header = require('../header.js'); 
 
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   19 December 2017
+; Modified By: Heather Peterson
+; Description: Exercise 3.2 - Arrays
+;===========================================
+*/ 
+
 // start program
 
 /*
@@ -20,81 +30,33 @@ var header = require('../header.js');
 */ 
 
 // functions 
-function match(A,B) {
-    if (A === B) {
-    return true;
-    }
-    else {
-    return false;
-    }
-}
 
-function logMismatch(Truck,Car) {
-    if (Truck !== Truck) {
-    return 'This is a match.';
-    }
-    else {
-    return 'Truck and Car do not match!';
-    }
-}
-
-function logMatch(Bike,Bike) {
-    if (Bike === Bike) {
-    return 'Bike and Bike do match!';
-    }
-    else {
-    return 'This is not a match.';
-    }
-}
-
-function logMismatch2(Four,Three) {
-    if (Four === Three) {
-        return 'This is a match.';
+function misMatch(c,d) {
+    var c = 1;
+    var d = 2;
+    if (c === d) {
+        return true;
+        console.log(match());
+        } else {
+        return false;
+        console.log(misMatch());
         }
-        else {
-        return 'Four and Three do not match!';
+    
+}
+
+function match(a,b) {
+    var a = 1;
+    var b = 1;
+    if (a === b) {
+        return true;
+        console.log(match());
+        } else {
+        return false;
+        console.log(misMatch());
         }
-    }
-
-
-// six (6) test variables 
-var msg ='';
-var quantity;
-var code;
-var price;
-var total;
-var name;
-quantity = 300;
-code = 1001;
-price = 20.99;
-total = quantity * price;
-name = code = 'Hammer';
-
+}
 
 // Output from the match() function... 
-console.log(match("A", "B"));
-console.log(match(2, 2)); 
-console.log(logMismatch("Truck", "Car"));
-console.log(logMatch("Bike","Bike"));
-console.log(logMismatch2("Four","Three"));
-
-// Conditional "if...else" statements. Include checks for all six (6) test variables 
-if (msg == quantity) {
-    console.log(quantity);
-} else if (msg !== code);{
-    console.log(code);
-}
-if (msg == price) {
-    console.log(price);
-} else if (msg == total);{
-    console.log(total);
-}
-if (msg == name) {
-    console.log(name,code);
-} else if (msg == code);{
-    console.log(code);
-}
-
-
-
-// end program 
+console.log(misMatch());
+console.log(match());
+ 
