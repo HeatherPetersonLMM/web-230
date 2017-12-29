6f695fcbbcb67ec9e8c075752eef3613e7842f5c
Heather Peterson <hpeterson@my.bellevue.edu>
2017-12-29 10:05:37 -0500

    More corrected code for exercise 3.2 -- Truck and Car do not match! and code comments

--- week-3/peterson-exercise-3.2.js
+++ week-3/peterson-exercise-3.2.js
@@ -30,28 +30,49 @@ var header = require('../header.js');
 
 // functions 
 console.log("// output from the match() function");
-function misMatch(a,b) {
-    if (a > b) {
-        return true;
+function misMatch(a,b) {  //calls the mismatch functions a and b
+    if (a > b) {  
+        return true;  //if a is greater than b, return true
         console.log(misMatch());
         } else {
-        return false;
+        return false;  //else if, return false
         console.log(match());
         }
     
 }
 
-function match(a,b) {
+function match(a,b) {  //calls the match functions a and b
     if (a === b) {
-        return true;
+        return true;  //if a is equal to b, return true
         console.log(match());
         } else {
-        return false;
+        return false;  //else if, return false
         console.log(misMatch());
         }
+}
+
+// six (6) test variables
+var t = "Truck";
+var c = "Car";
+var b = "Bike";
+var i = "Bike";
+var f = "Four";
+var h = "Three";
+
+function matchVeh(t,c) {
+    console.log("");
+    console.log("// output from the if...else blocks");
+if (t === c) {
+    return true;
+    console.log(match(t,c));
+    } else {
+    return "Truck and Car do not match!";
+    console.log(misMatch(t,c));
+}
 }
 
 // Output from the match() function... 
 console.log(misMatch());
 console.log(match());
+console.log(matchVeh(t,c));
  
