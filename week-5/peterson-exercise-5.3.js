2e79f14701c345163bd5d5bb9110ad5440af11d7
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-09 07:21:10 -0500

    added header and starter code

---
+++ week-5/peterson-exercise-5.3.js
@@ -0,0 +1,12 @@
+var header = require('../header.js'); 
+
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   09 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 5.2 - Built-in Functions
+;===========================================
+*/  
+ 
