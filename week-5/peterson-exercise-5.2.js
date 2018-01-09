e7e59eb3d66f75ae160fec7011eeb4bc198ea235
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-09 16:55:50 -0500

    used ForEach() method to complete assignment

---
+++ week-5/peterson-exercise-5.2.js
@@ -0,0 +1,37 @@
+var header = require('../header.js'); 
+
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   09 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 5.2 - Built-in Functions
+;===========================================
+*/  
+ 
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 5.2
+ Today's Date
+
+ Oysters
+ Shrimp
+ Steak
+ Tacos
+ Sushi
+
+ */
+
+// start program
+
+var foods = ["Pho", "Tacos", "Sushi", "Steak", "Hibachi"];  //An array of favorite foods
+var sum = 0;  // Start at 0
+foods.forEach(function(num) {  //envokes each specified function
+    console.log(num)  //displays array elements, starting with the first
+});
+
+
+// end program
