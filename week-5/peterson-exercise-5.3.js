3d4037d7b497cf16391365f434d37ddbe4664340
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-09 19:21:22 -0500

    added Array-Like object collection of composers

---
+++ week-5/peterson-exercise-5.3.js
@@ -0,0 +1,67 @@
+var header = require('../header.js'); 
+
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   09 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 5.3 - Object Collections
+;===========================================
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 5.3
+ Today's Date
+
+ -- COMPOSERS --
+ Last Name: Beethoven, Genre: Classical, Rating: 8
+ Last Name: Mozart, Genre: Classical, Rating: 10
+ Last Name: Bach, Genre: Classical, Rating: 9
+ Last Name: Haydn, Genre: Classical, Rating: 6
+ Last Name: Schubert, Genre: Classical, Rating: 5
+
+ */
+
+// start program
+var famousComposers = [
+    {
+        firstName: 'Ludwig',
+        lastName: 'Beethoven',
+        genre:'Classical',
+        rating: '8'
+    },
+    {
+        firstName: 'Johannes',
+        lastName: 'Mozart',
+        genre: 'Classical',
+        rating: '10'
+    },
+    {
+        firstName: 'Johann',
+        lastName: 'Bach',
+        genre:'Classical',
+        rating: '6'
+    },
+    {
+        firstName:'Franz',
+        lastName:'Haydn',
+        genre: 'Classical',
+        rating: '6'
+    },
+    {
+        firstName: 'Franz',
+        lastName:'Schubert',
+        genre: 'Classical',
+        rating: '5'
+    },
+];
+
+
+
+
+
+
+// end program
