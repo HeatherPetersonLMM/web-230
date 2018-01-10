eb938a45278b5fd8a4a8bd493af924cd7e63bc7c
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-10 16:46:22 -0500

    changed to assignment 5.4

--- header.js
+++ header.js
@@ -23,7 +23,7 @@ if (dd<10) {
   }
   
   today = mm + '-' + dd + '-' + yyyy
-  var assignmentNum = "Exercise 5.3";
+  var assignmentNum = "Assignment 5.4";
   
   var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
       + assignmentNum + "\nDate: " + today;
