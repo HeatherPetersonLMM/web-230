cb76005fc71d40eb49da6992c8760112f71024f3
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-10 16:44:48 -0500

    added starter code and code attribution

---
+++ week-5/peterson-assignment-5.4.js
@@ -0,0 +1,88 @@
+var header = require('../header.js');
+
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   10 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 5.4 - Filtering/Reducing Complex Data Structures
+;===========================================
+*/ 
+
+/*
+Expected output:
+
+ FirstName LastName
+ Assignment 5.4
+ Today's Date
+
+ -- COMPOSER BY RATING --
+ Rating: 8
+ Composer: Beethoven
+
+ Rating: 10
+ Composer: Mozart
+
+ Rating: 9
+ Composer: Bach
+
+ Rating: 6
+ Composer: Haydn
+
+ Rating: 5
+ Composer: Schubert
+
+ -- COMPOSER BY GENRE --
+ Genre: Classical
+ Composer: Beethoven
+
+ Genre: Classical
+ Composer: Mozart
+
+ Genre: Classical
+ Composer: Bach
+
+ Genre: Classical
+ Composer: Haydn
+
+ Genre: Classical
+ Composer: Schubert
+ */
+
+// start program
+
+var famousComposers = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+];
+
+var comnposersByRating = famousComposers.map(function(composer) {
+
+});
+
+var composersByGenre = famousComposers.map(function(composer) {
+
+});
+
+console.log("-- COMPOSER BY RATING --");
+
+
+console.log("-- COMPOSER BY GENRE --");
+
+
+
+// end program
