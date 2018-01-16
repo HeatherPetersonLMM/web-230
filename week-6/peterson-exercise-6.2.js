34c041218101355967ebb9586736cab329adf0e5
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-16 07:13:59 -0500

    added starter code for ex. 6.2

---
+++ week-6/peterson-exercise-6.2.js
@@ -0,0 +1,25 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 6.2
+ Today's Date
+
+ Catch clause: <Your message>
+ Finally clause reached: End of program â€¦
+
+ */
+
+// start program
+
+try {
+
+} catch (err) {
+
+} finally {
+
+}
+
+// end program
