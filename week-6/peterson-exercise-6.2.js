a379f8eb51e6a5aa251d9244fbe3787247a2f086
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-16 21:25:10 -0500

    starter code example

--- week-6/peterson-exercise-6.2.js
+++ week-6/peterson-exercise-6.2.js
@@ -14,12 +14,21 @@ var header = require('../header.js');
 
 // start program
 
+
 try {
+    var x = 0;
+    var y = 1;
+
+    var sum = y / x;
+
+    if (sum == Infinity) throw "DivideByZeroException";
+
+    console.log(sum);
 
 } catch (err) {
-
+    console.log("Catch clause: " + err);
 } finally {
-
+    console.log("Finally clause reached: End of program!");
 }
 
 // end program
