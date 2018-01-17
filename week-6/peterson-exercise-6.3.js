a43355ac8427efcf4960dd7be21ca55e27fd206a
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-17 18:04:36 -0500

    finalized code for ex 6.3 and code comments

--- week-6/peterson-exercise-6.3.js
+++ week-6/peterson-exercise-6.3.js
@@ -1,6 +1,17 @@
 var header = require('../header.js');
 
 /*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   17 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 6.3 - Object Literals
+;===========================================
+/*
+
+
+/*
  Expected output:
 
  FirstName LastName
@@ -13,10 +24,50 @@ var header = require('../header.js');
 
 // start program
 
-var ticket = {
+var ticket = {  //object literal for ticketing system with three fields and default values
+    id: 1,
+    name: "Sue Jefferson",
+    requester: "Heather Peterson",
+
+    get ticketId() {  //getter - passes no arguments
+
+        return this.id; //return the value of the property access expression
+
+    },
+
+    set ticketId(val) { //setter - passing the value of the ticketId
+
+        this.id = val; //returning the value of the setter
+
+    },
+
+    get ticketName() {
+
+        return this.name;
+
+    },
+
+    set ticketName(val) {
+
+        this.name = val;
+
+    },
+
+    get ticketRequester() {
+
+        return this.requester;
+
+    },
+
+    set ticketRequester(val) {
+
+        this.requester = val;
+
+    }
 
 };
 
+console.log("{id: " + ticket.ticketId + ", " + "name: " + ticket.ticketName + ", " + "requester: " + ticket.ticketRequester + "}"); //output in one concatenated string
 
 
 // end program
