f6bf177b42a97d3cf4da299b79518cde7e485c3d
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-17 17:21:34 -0500

    added starter code

---
+++ week-6/peterson-exercise-6.3.js
@@ -0,0 +1,22 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 6.3
+ Today's Date
+
+ {id: <ticket id>, name: <ticket name>, requester: <your name>}
+
+ */
+
+// start program
+
+var ticket = {
+
+};
+
+
+
+// end program
