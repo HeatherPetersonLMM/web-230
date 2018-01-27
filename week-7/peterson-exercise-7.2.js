83a0fbd0a927c9d79666a347601ab22d50a436b3
Heather Peterson <hpeterson@my.bellevue.edu>
2018-01-27 13:47:26 -0500

    Added starter code, header and code attribution

---
+++ week-7/peterson-exercise-7.2.js
@@ -0,0 +1,31 @@
+var header = require('../header.js');
+/*
+
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   27 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 7.2 - Constructor Functions
+;===========================================
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 7.2
+ Today's Date
+
+ 1 Thomas Edison Software Engineer
+ 2 Benjamin Franklin Programmer
+ 3 Nikola Tesla Project Manager
+ 4 Charles Babbage Product Manager
+ 5 Alexander Bell Business Analyst
+
+ */
+
+// start program
+
+
+
+// end program
